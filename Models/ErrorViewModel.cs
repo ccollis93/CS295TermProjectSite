3eda142d2acc6be451f5ca235368b447006d659c
Christian <collisc@my.lanecc.edu>
2021-12-05 12:52:23 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameBaseSIte.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
