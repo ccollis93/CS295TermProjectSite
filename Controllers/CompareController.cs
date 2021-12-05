b22cfe6a448fd7f4972469b999728a5b2cda1261
Christian <collisc@my.lanecc.edu>
2021-12-05 14:25:20 -0800

    innitial commut. added stubs for Views and Controllers. Edited _Layout, style.ccc, and ProfileIndex

---
+++ Controllers/CompareController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameBaseSIte.Controllers
+{
+    public class CompareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameBaseSIte.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult ProfileIndex()
+        {
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
